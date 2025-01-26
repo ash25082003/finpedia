675253450f938356b889bdd1d05049d1bdfa3483
Ashish <ashish_b@hs.iitr.ac.in>
2025-01-26 17:28:31 +0530

    error handling for connectDB

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ dotenv.config(
 )
 
 connectDB()
+.then(() => {
+    app.on("error",() => {
+        console.log("Err: ",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000,
+        () => {
+            console.log(`Server is running at port: ${process.env.PORT}`); 
+        }
+    )
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err)
+})
 
 
 
